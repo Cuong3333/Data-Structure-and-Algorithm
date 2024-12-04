4d162c766bcc74d0319666a11b1516d4319e8ef0
cuong <cn3576167@gmail.com>
2024-12-04 11:17:19 +0700

    edit code

--- JavaApplication4/src/studens/Main.java
+++ JavaApplication4/src/studens/Main.java
@@ -16,11 +16,11 @@ public class Main {
             System.out.println("1. Add Student");
             System.out.println("2. Edit Student");
             System.out.println("3. Delete Student");
-            System.out.println("4. Search Student by ID");
+            System.out.println("4. Sort Students by Marks");
             System.out.println("5. View All Students");
-            System.out.println("6. Sort Students by Marks");
-            System.out.println("7. Search Student by Name");
-            System.out.println("8. Exit");
+            System.out.println("6. Search Student by ID");
+//            System.out.println("7. Search Student by Name");
+            System.out.println("7. Exit");
             System.out.print("Enter your choice: ");
 
             int choice = scanner.nextInt();
@@ -38,15 +38,15 @@ public class Main {
                     String id = scanner.nextLine();
                     manager.deleteStudent(id);
                 }
-                case 4 -> {
+                case 6 -> {
                     System.out.print("Enter student ID to search: ");
                     String id = scanner.nextLine();
                     manager.findStudentById(id);
                 }
                 case 5 -> manager.displayAllStudents();
-                case 6 -> manager.sortByMarks();
-                case 7 -> manager.searchStudentByName();
-                case 8 -> {
+                case 4 -> manager.sortByMarks();
+//                case 7 -> manager.searchStudentByName();
+                case 7 -> {
                     System.out.println("Exiting...");
                     return;
                 }
